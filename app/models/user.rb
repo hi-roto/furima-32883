315b9f7d3da945cb4aeda3f4c57a5e100f073f29
hi-roto <hirostudy2020@gmail.com>
2021-02-13 16:43:29 +0900

    ストロングパラメータ追記、バリデーションの追記、ビューファイルのform_with保存先カラムの指定
    １application_contorolleにデバイスのストロンづパラメータを定義
    ２userモデルにバリデーションを追記
    ３form_withの各入力欄の保存先を指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :last_name, :first_name, :last_name_kana, :birth_day, presence: true
-  
+  validates :nickname, :email, uniqueness: true
+  VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD } 
   
 end
