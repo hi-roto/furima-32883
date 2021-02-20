8a691d79a2e0647b8a6b63cdbdd9869426b9dbd2
hi-roto <hirostudy2020@gmail.com>
2021-02-20 15:01:43 +0900

    Active Hash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
 belongs_to :user
+has_one_attached :image
+
+validates :image, :name, :introduction, :price, presence: true
+validates :category_id, :item_status_id, :postage_payer_id, :prefecture_id, :preparation_day_id, numericality: { other_than: 1 }
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category
+belongs_to :item_status
+belongs_to :postage_payer
+belongs_to :prefecture
+belongs_to :preparation_day
 
 end
