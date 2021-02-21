6b01483f79acd9ee5e07a3c1dae5ccd682721294
hi-roto <hirostudy2020@gmail.com>
2021-02-21 13:31:16 +0900

    itemコントローラのインスタンス変数名修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @Item = Item.all
+    @items = Item.all
   end
 
   def new
