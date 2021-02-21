a9ba9847e614b12faf8bb579590f8a61d11041fa
hi-roto <hirostudy2020@gmail.com>
2021-02-21 21:49:44 +0900

    不要な説明書きの削除　ビューの条件分岐の修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', () => {
-  // 読み込まれたら以下を実行する
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    // 金額入力欄に金額を入力したら、以下を実行する
     const inputValue = priceInput.value;
     const addTax = document.getElementById("add-tax-price");
     const taxNum = Math.floor(inputValue * 0.1);
