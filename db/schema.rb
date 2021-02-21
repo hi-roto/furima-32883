51702108c5414ba3e1dd540be43ea73bdc532ca1
hi-roto <hirostudy2020@gmail.com>
2021-02-21 13:33:14 +0900

    itemモデルのカラムが不足していたので、prefectureカラムを追記

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_130555) do
     t.text "introduction", null: false
     t.integer "category_id", null: false
     t.integer "item_status_id", null: false
+    t.integer "postage_payer_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "preparation_day_id", null: false
     t.integer "price", null: false
