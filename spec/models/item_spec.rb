f8d87d4ea53a7012de1957bd55145afdbace0b0f
hi-roto <hirostudy2020@gmail.com>
2021-02-22 21:08:28 +0900

    idが1のとき登録できないテストを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -53,6 +53,31 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+      it 'カテゴリーの情報がid:1の時は保存できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
+      end
+      it '商品の状態がid:1の時は保存できないこと' do
+        @item.item_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item status must be other than 1')
+      end
+      it '配送料の負担についての情報がid:1の時は保存できないこと' do
+        @item.postage_payer_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Postage payer must be other than 1')
+      end
+      it '発送元の地域についての情報がid:1の時は保存できないこと' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
+      end
+      it '価格についての情報が必須であること' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
       it '価格の範囲が、¥300~¥9,999,999の間であること(300未満の場合)' do
         @item.price = 299
         @item.valid?
