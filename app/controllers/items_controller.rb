b74b9791fff33909388d92353f2dee70c1bd5a87
hi-roto <hirostudy2020@gmail.com>
2021-02-28 20:48:12 +0900

    itemコントローラー内の同じインスタンス変数を１つのメソッドにまとめる修正
    show,edit,updateのアクションに定義したインスタンス変数@itemとインスタンスをset_itemというメソッドにまとめる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_root, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :edit
     else
@@ -45,5 +43,9 @@ class ItemsController < ApplicationController
 
   def move_to_root
     redirect_to root_path unless current_user.id == Item.find(params[:id]).user.id
+  end
+  
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
