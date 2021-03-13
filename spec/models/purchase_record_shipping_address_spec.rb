df67d269f6513bf2e3d2147fb97a0fbba15661ab
hi-roto <hirostudy2020@gmail.com>
2021-03-13 21:28:06 +0900

    テストコードの修正

--- spec/models/purchase_record_shipping_address_spec.rb
+++ spec/models/purchase_record_shipping_address_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PurchaseRecordShippingAddress, type: :model do
   before do
-    @purchase_record_shipping_address = FactoryBot.build(:purchase_record_shipping_address)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @purchase_record_shipping_address = FactoryBot.build(:purchase_record_shipping_address, user_id: @user.id, item_id: @item.id)
+    sleep 0.1
   end
 
   describe '商品購入機能' do
